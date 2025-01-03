1e2696dca9543dc77e7a0296c3282b3cec02eb3c
Andres Vega <aevega1991@gmail.com>
2025-01-03 17:07:05 -0300

    feat: :sparkles: S10L108 Crear sesion de pago
    Generando un payment link de Stripe, al pagarlo manualmente redirige a la ruta de success

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -5,4 +5,29 @@ import Stripe from 'stripe';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession() {
+    const session = await this.stripe.checkout.sessions.create({
+      metadata: {
+        order_id: 'ord_1I5x0o2eZvKYlo2C',
+      },
+      currency: 'usd',
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            unit_amount: 1000,
+            product_data: {
+              name: 'T-Shirt',
+            },
+          },
+          quantity: 2,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:3003/payments/success',
+      cancel_url: 'http://localhost:3003/payments/cancelled',
+    });
+    return session;
+  }
 }
